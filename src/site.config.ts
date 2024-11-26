c231ebb3a27a262c5395483fdbc22348bcb0b8ff
dmitri <popov.d@flgchina.ru>
2024-11-26 16:30:15 +0500

    fix: fixed config

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,11 +3,11 @@ import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: 'SRLEOM',
+	author: 'Dmitriy',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'astro-theme-resume',
+	title: 'Резюме',
 	// Meta property used as the default description meta property
-	description: 'The official Astro Resume Theme',
+	description: 'Резюме frontend разработчика',
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: 'en-GB',
 	// Meta property, found in src/components/BaseHead.astro L:42
